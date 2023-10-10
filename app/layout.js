8406d7502f32e23dbb7616becdf796ce89fcdda3
MatiasDAppella <appellamatias@gmail.com>
2023-10-10 16:12:54 -0300

    tailwindcss added

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,10 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Frontend Mentor #1',
+  description: 'Built in Next.js 13, using Tailwind'
+}
+
 export default function RootLayout({ children }){
   return (<html lang="en">
     <body>{children}</body>
